01bf961cf2080561b0c81ba551a4b3994a9caa6d
Quinn <armstrongq22@gmail.com>
2020-07-12 10:55:16 -0500

    debugging

--- server/passport/SignupStrategy.js
+++ server/passport/SignupStrategy.js
@@ -8,11 +8,12 @@ const salt = bcrypt.genSaltSync(10);
 const SignupStrategy = new Strategy({passReqToCallback: true, usernameField: 'email'}, function(req, email, password, done) {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
+    console.log('4.1');
     
     User.findOne({email}).lean().exec((err, user) => {
         if(err) return done(err, null);
         if(user) return done('User already exists', null);
-        
+        console.log('4.2');
         const encryptedPassword = bcrypt.hashSync(password, salt);
         let newUser = new User({
             firstName: firstName,
@@ -20,9 +21,10 @@ const SignupStrategy = new Strategy({passReqToCallback: true, usernameField: 'em
             email,
             password: encryptedPassword,
         })
-
+        console.log('4.3');
         newUser.save((error, inserted) => {
             if(error) return done(error, null);
+            console.log('4.4');
             return done(null, inserted);
         });
     });
